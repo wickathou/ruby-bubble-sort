77dded00b4f2c97bc995da3b9fe9f4d765a56c2e
Christian Salazar <calaca89@gmail.com>
2019-10-24 11:50:50 -0500

    bubble_sort function done

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,4 +2,18 @@
 
 def bubble_sort(arr)
     ite = 0
-    for  ite in 0...arr.size
+    length = arr.size - 1
+    while ite < length do
+        for n in 0...length do
+            key = arr[n]
+            if arr[n + 1] < arr[n] then
+                arr[n]  = arr[n + 1]
+                arr[n + 1] = key
+            end
+        end
+        ite += 1
+    end
+    puts arr
+end
+
+bubble_sort([67,3,89,6,21,43])
