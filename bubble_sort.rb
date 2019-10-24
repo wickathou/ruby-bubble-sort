5dc3ab6e2232d61f3c25d5425d19d6f25c8c9769
Christian Salazar <calaca89@gmail.com>
2019-10-24 13:07:49 -0500

    Added a test brach

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,12 @@
 
 
-def bubble_sort(arr)
+def bubble_sort(arr, &block)
     ite = 0
     length = arr.size - 1
     while ite < length do
         for n in 0...length do
-            key = arr[n]
-            if arr[n + 1] < arr[n] then
-                arr[n]  = arr[n + 1]
-                arr[n + 1] = key
+            if yield(arr[n], arr[n + 1]) > 0
+                arr[n], arr[n + 1] = arr[n + 1], arr[n]
             end
         end
         ite += 1
@@ -16,4 +14,28 @@ def bubble_sort(arr)
     puts arr
 end
 
-bubble_sort([67,3,89,6,21,43])
+# bubble_sort([67,3,89,6,21,43])
+
+def bubble_sort_by(series, &block)
+    finished = false
+    # while finished == false
+    #     finished = true
+        (series.size - 1).times do |i|
+    #         if yield(series[i], series[i + 1]) > 0
+    #             series[i], series[i + 1] = series[i + 1], series[i]
+    #             finished = false
+    #             break
+    #         end
+        puts yield(series[1], series[1])
+        end
+
+    # end
+
+    p series
+end
+
+
+
+bubble_sort(["hi", "hello", "hey"]) do |left, right|
+    left.length + right.length
+end
