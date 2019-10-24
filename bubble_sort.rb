e14cc1c5a781b07aef6cd156f241d9895f23e742
wickathou <javidx@gmail.com>
2019-10-24 12:19:44 -0500

    Added bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -17,3 +17,39 @@ def bubble_sort(arr)
 end
 
 bubble_sort([67,3,89,6,21,43])
+
+
+
+
+def bubble_sort_by(arr)
+    ite = 0
+    check = 3
+    length = arr.size - 1
+    while ite < length do
+        for n in 0...length do
+            key = arr[n]
+            yield arr[n], arr[n+1]
+
+            arr.each do |a, b|
+                if a.size < b.size
+                    check = -1
+                elsif a.size == b.size
+                    check = 0
+                elsif a.size > b.size
+                    check = 1
+                end
+            
+
+
+                
+            # end
+
+
+        end
+        ite += 1
+    end
+    puts arr
+end
+
+bubble_sort([67,3,89,6,21,43])
+
