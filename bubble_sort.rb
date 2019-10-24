b02b31b6bfc91edc773869e2df1a0137473a784d
Christian Salazar <calaca89@gmail.com>
2019-10-24 15:28:48 -0500

    fixed linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,38 +1,34 @@
-
-
 def bubble_sort(arr)
-    ite = 0
-    length = arr.size - 1
-    while ite < length do
-        for n in 0...length do
-            key = arr[n]
-            if arr[n + 1] < arr[n] then
-                arr[n]  = arr[n + 1]
-                arr[n + 1] = key
-            end
-        end
-        ite += 1
+  ite = 0
+  length = arr.size - 1
+  while ite < length
+    for n in 0...length do
+      key = arr[n]
+      if arr[n + 1] < arr[n]
+        arr[n] = arr[n + 1]
+        arr[n + 1] = key
+      end
     end
-    puts arr
+    ite += 1
+  end
+  puts arr
 end
 
-bubble_sort([67,3,89,6,21,43])
+bubble_sort([67, 3, 89, 6, 21, 43])
 
 def bubble_sort_by(arr)
-    ite = 0
-    length = arr.size - 1
-    while ite < length do
-        for n in 0...length do
-            if yield(arr[n], arr[n+1]) > 0
-                arr[n], arr[n+1] = arr[n+1], arr[n]
-            end
-        ite += 1
-        end
+  ite = 0
+  length = arr.size - 1
+  while ite < length
+    for n in 0...length do
+      arr[n], arr[n + 1] = arr[n + 1], arr[n] if yield(arr[n], arr[n + 1]).positive?
     end
-    puts arr
+    ite += 1
+  end
+  puts arr
 end
 
-bubble_sort_by(['a','aa','aaaa']) do |a, b|
-    a.size - b.size
+bubble_sort_by(%w(aaa aaaa aa)) do |a, b|
+  a.size - b.size
 end
 
